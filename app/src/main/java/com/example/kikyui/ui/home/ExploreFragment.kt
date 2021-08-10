bc9fc33fbf68e4435ad96dd5bcc20904d7df561b
Raeid <raeidmir@hotmail.com>
2021-08-10 15:25:28 +0500

    camera issues dialogues added

--- app/src/main/java/com/example/kikyui/ui/home/ExploreFragment.kt
+++ app/src/main/java/com/example/kikyui/ui/home/ExploreFragment.kt
@@ -1,14 +1,29 @@
 package com.example.kikyui.ui.home
 
+import android.Manifest
+import android.app.ActionBar
+import android.app.Dialog
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.kikyui.*
 import com.example.kikyui.databinding.FragmentExploreBinding
+import kotlinx.android.synthetic.main.fragment_explore.*
 
 class ExploreFragment : Fragment() {
 
@@ -33,18 +48,31 @@ class ExploreFragment : Fragment() {
         val btnProfile = binding.btnProfile
         val btnHistory= binding.btnHistory
         val btnGddess=binding.btnGddes
+        val btnGender = binding.btnGender
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            activity?.let {
+                ActivityCompat.requestPermissions(
+                    it, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+            }
+        }
+
 
         btnProfile.setOnClickListener{
             val intent = Intent(activity,AccountSettingActivity::class.java)
             startActivity(intent)
         }
         btnHistory.setOnClickListener {
-            val intent = Intent(activity, IncomingCallActivity::class.java)
+            val intent = Intent(activity, HistoryGalleryActivity::class.java)
             startActivity(intent)
         }
         btnGddess.setOnClickListener {
             val intent=Intent(activity,GddesActivity::class.java)
             startActivity(intent)
+        }
+        btnGender.setOnClickListener{
+                showDialog("Gender Preferences")
         }
 //
 
@@ -59,4 +87,103 @@ class ExploreFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun showDialogGender(title: String) {
+        val dialog = activity?.let { Dialog(it) }
+        dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog?.setCancelable(true)
+        dialog?.setContentView(R.layout.custom_gender_dialog)
+        val width = (resources.displayMetrics.widthPixels * 0.90).toInt()
+        val height = (resources.displayMetrics.heightPixels * 0.90).toInt()
+        dialog?.window?.setLayout(width,ActionBar.LayoutParams.WRAP_CONTENT);
+//        val body = dialog?.findViewById(R.id.body) as TextView
+//        body.text = title
+//        val yesBtn = dialog.findViewById(R.id.yesBtn) as Button
+//        val noBtn = dialog.findViewById(R.id.noBtn) as TextView
+//        yesBtn.setOnClickListener {
+//            dialog.dismiss()
+//        }
+//        noBtn.setOnClickListener { dialog.dismiss() }
+        dialog?.show()
+
+    }
+    private fun showDialog(title: String) {
+        val dialog = activity?.let { Dialog(it) }
+        dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog?.setCancelable(true)
+        dialog?.setContentView(R.layout.custom_report_dialog)
+        val width = (resources.displayMetrics.widthPixels * 0.90).toInt()
+        val height = (resources.displayMetrics.heightPixels * 0.90).toInt()
+        dialog?.window?.setLayout(width,ActionBar.LayoutParams.WRAP_CONTENT);
+//        val body = dialog?.findViewById(R.id.body) as TextView
+//        body.text = title
+//        val yesBtn = dialog.findViewById(R.id.yesBtn) as Button
+//        val noBtn = dialog.findViewById(R.id.noBtn) as TextView
+//        yesBtn.setOnClickListener {
+//            dialog.dismiss()
+//        }
+//        noBtn.setOnClickListener { dialog.dismiss() }
+        dialog?.show()
+
+    }
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+
+         activity?.baseContext?.let {
+             ctx->it
+            ContextCompat.checkSelfPermission(
+                ctx, it)
+        } == PackageManager.PERMISSION_GRANTED
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(activity,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                activity?.finish()
+            }
+        }
+    }
+    private fun startCamera() {
+        val cameraProviderFuture = activity?.let { ProcessCameraProvider.getInstance(it) }
+
+        cameraProviderFuture?.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewFinder.surfaceProvider)
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(activity))
+    }
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
 }

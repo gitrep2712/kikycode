465c3f087cc5a3e07a3e61a99f0943a3274b5fef
gitrep2712 <87866394+gitrep2712@users.noreply.github.com>
2021-09-13 08:30:04 +0530

    Revert "Gender bottom sheet"

--- app/src/main/java/com/example/kikyui/adapters/SmallImageAdapter.kt
+++ app/src/main/java/com/example/kikyui/adapters/SmallImageAdapter.kt
@@ -31,7 +31,7 @@ class SmallImageAdapter(val listener:SmallImageOnclick,val context: Context): Re
     }
 
     override fun getItemCount(): Int {
-        return 20
+        return 10
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
